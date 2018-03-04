c17a95dee340148314fc803aea69dbbe87f22ec9
Jonathan Porter <mail@j.porter.name>
2018-03-04 11:13:48 -0800

    Rewrote percent decoding to not use array

--- InternetStandards.WHATWG.Url/PercentEncodedBytes.cs
+++ InternetStandards.WHATWG.Url/PercentEncodedBytes.cs
@@ -11,27 +11,37 @@ namespace InternetStandards.WHATWG.Url
 
         public static IEnumerable<byte> PercentDecode(IEnumerable<byte> input)
         {
-            var inputArray = input.ToArray();
-
-            var index = 0;
+            var twoByteBuffer = new byte[2];
 
             bool NotInRange()
             {
-                var nextTwoBytesAfter = inputArray.Skip(index + 1).Take(2).ToArray();
-                return nextTwoBytesAfter.Length == 2 && nextTwoBytesAfter.All(b =>
+                return twoByteBuffer.All(b =>
                     !(b >= 0x30 && b <= 0x39 || b >= 0x41 && b <= 0x46 || b >= 0x61 && b <= 0x66));
             }
 
-            for (; index < inputArray.Length; index++)
+            using (var enumerator = input.GetEnumerator())
             {
-                if (inputArray[index] != 0x25 || NotInRange())
+                while (enumerator.MoveNext())
                 {
-                    yield return inputArray[index];
-                }
-                else
-                {
-                    var bytePoint = Encoding.UTF8.GetString(new[] {inputArray[++index], inputArray[++index]});
-                    yield return Convert.ToByte(bytePoint, 16);
+                    if (enumerator.Current != 0x25)
+                        yield return enumerator.Current;
+
+                    int twoByteBufferLength;
+                    for (twoByteBufferLength = 0;
+                        twoByteBufferLength < 2 && enumerator.MoveNext();
+                        twoByteBufferLength++)
+                        twoByteBuffer[twoByteBufferLength] = enumerator.Current;
+
+                    if (twoByteBufferLength != 2 || NotInRange())
+                    {
+                        yield return 0x25;
+                        for (var index = 0; index < twoByteBufferLength; index++)
+                            yield return twoByteBuffer[index];
+                    }
+                    else
+                    {
+                        yield return Convert.ToByte(Encoding.UTF8.GetString(twoByteBuffer), 16);
+                    }
                 }
             }
         }
